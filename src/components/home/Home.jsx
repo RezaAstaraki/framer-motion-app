c03998d4c01d6db9ac74130ea000474d2e02fded
Omidi-a <omidiazam325@gmail.com>
2024-02-07 01:24:34 +0330

    add Home component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import "./home.scss";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome to Pizza Joint</h2>
+
+      <a href="/base">
+        <button>Create Your Pizza</button>
+      </a>
+    </div>
+  );
+}
